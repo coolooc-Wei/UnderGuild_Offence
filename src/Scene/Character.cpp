a7147805548813caffe5d4a3b6c5c43834ff1baa
coolooc-Wei <147019463+coolooc-Wei@users.noreply.github.com>
2026-03-27 10:02:42 +0800

    fix foolish error (#12)
    * fix foolish error.

--- src/Scene/Character.cpp
+++ src/Scene/Character.cpp
@@ -1,47 +1,54 @@
 #include "Scene/Character.hpp"
+#include "Scene/Weapon.hpp"
+#include "Scene/StatusEffect.hpp"
+
 namespace UGO::Scene {
 
-Character::Character(HpValue maxHP, HpValue attackPower)
-    : m_maxHP(maxHP), m_currentHP(maxHP), m_attackPower(attackPower) {}
+    Character::Character(HpValue maxHP, HpValue attackPower)
+        : m_maxHP(maxHP), m_currentHP(maxHP), m_attackPower(attackPower) {}
+    Character::~Character() = default;
 
-HpValue Character::GetMaxHP() const { return m_maxHP; }
-HpValue Character::GetCurrentHP() const { return m_currentHP; }
-HpValue Character::GetAttackPower() const { return m_attackPower; }
+    HpValue Character::GetMaxHP() const { return m_maxHP; }
+    HpValue Character::GetCurrentHP() const { return m_currentHP; }
+    HpValue Character::GetAttackPower() const { return m_attackPower; }
 
-void Character::SetMaxHP(HpValue newMaxHP) {
-    /* TODO: Check if maxHP is valid
+    void Character::SetMaxHP(HpValue newMaxHP) {
+        /* TODO: Check if maxHP is valid
+        */
+        assert(newMaxHP >= 0);
+        m_maxHP = newMaxHP;
+    }
+
+    /* TODO: Damage and Heal amount should be affected by effects and character's attributes.
     */
-    assert(newMaxHP >= 0);
-    m_maxHP = newMaxHP;
-}
+    void Character::TakeDamage(HpValue amount) {
+        assert(amount >= 0);
+        if (m_currentHP <= 0) { return; }
 
-/* TODO: Damage and Heal amount should be affected by effects and character's attributes.
-*/
-void Character::TakeDamage(HpValue amount) {
-    assert(amount >= 0);
-    if (m_currentHP <= 0) { return; }
+        if (amount > m_currentHP) {
+            m_currentHP = 0;
+        }
+        else {
+            m_currentHP -= amount;
+        }
+    }
+    void Character::Heal(HpValue amount) {
+        assert(amount >= 0);
 
-    if (amount > m_currentHP) {
-        m_currentHP = 0;
+        if (m_currentHP + amount > m_maxHP) {
+            m_currentHP = m_maxHP;
+        }
+        else {
+            m_currentHP += amount;
+        }
     }
-    else {
-        m_currentHP -= amount;
+    void Character::SetAttackPower(HpValue attackPower) {
+        /* TODO: Check if attackPower is valid
+        */
+        m_attackPower = attackPower;
     }
-}
-void Character::Heal(HpValue amount) {
-    assert(amount >= 0);
 
-    if (m_currentHP + amount > m_maxHP) {
-        m_currentHP = m_maxHP;
-    }
-    else {
-        m_currentHP += amount;
-    }
-}
-void Character::SetAttackPower(HpValue attackPower) {
-    /* TODO: Check if attackPower is valid
-    */
-    m_attackPower = attackPower;
-}
+    void Character::Update() {}
+    void Character::OnDraw() {}
 
 }
--- src/Scene/Weapon.cpp
+++ src/Scene/Weapon.cpp
@@ -1,27 +1,33 @@
 #include "Scene/Weapon.hpp"
+#include "Scene/Character.hpp"
+#include "Scene/StatusEffect.hpp"
 
 namespace UGO::Scene {
+
+    Weapon::Weapon(WeaponData* data) : m_data(data) {}
+    Weapon::~Weapon() = default;
 
     std::string Weapon::GetName() const { return m_data->name; }
     WeaponType Weapon::GetType() const { return m_data->type; }
     HpValue Weapon::GetAttackPower() const { return m_data->attackPower; }
     Core::TimeStep Weapon::GetCooldownTime() const { return m_data->cooldownTime; }
 
-    void Weapon::SetData(WeaponData* data) {
-
+    void Weapon::SetData(const WeaponData* data) {
+        (void) data;
     }
     void Weapon::SetName(const std::string& name) {
         m_data->name = name;
     }
-    void Weapon::SetType(WeaponType type) {
+    void Weapon::SetType(const WeaponType type) {
         m_data->type = type;
     }
-    void Weapon::SetAttackPower(HpValue attackPower) {
+    void Weapon::SetAttackPower(const HpValue attackPower) {
         assert(attackPower >= 0);
         m_data->attackPower = attackPower;
     }
-    void Weapon::SetCooldownTime(Core::TimeStep cooldownTime) {
+    void Weapon::SetCooldownTime(const Core::TimeStep cooldownTime) {
         assert(cooldownTime >= 0);
         m_data->cooldownTime = cooldownTime;
     }
+
 }
